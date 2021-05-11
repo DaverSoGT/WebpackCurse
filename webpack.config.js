310019ed4b34ab42a46a40ef8487c9912b04f2fe
David Herrera <jodaniheso@gmail.com>
2021-05-10 23:59:11 -0600

    Agregada optimización de archivo js, css y fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
     output : {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[contenthash].js'
     },
     resolve: {
         extensions: ['.js']
@@ -41,7 +42,7 @@ module.exports = {
                     options: {
                         limit: 10000,
                         mimetype: 'application/font-woff',
-                        name: '[name].[ext]',
+                        name: '[name].[contenthash].[ext]',
                         outputPath: './assets/fonts/',
                         publicPath: './assets/fonts/',
                         esModule: false
@@ -56,6 +57,14 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }),
-        new MiniCssExtractPlugin(),
-    ]
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin()
+        ]
+    }
 }
